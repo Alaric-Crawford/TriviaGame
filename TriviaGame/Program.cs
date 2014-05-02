efe83b19f4a191ee7607ee28f65f9484a355f47d
Dustin Kraft <kraftdr@gmail.com>
2014-05-02 11:43:23 -0600

    Initial Check In
    Set up for the Trivia Game application

---
+++ TriviaGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static List<Trivia> GetTriviaList()
+        {
+            //Get Contents from the file.  Remove the special char "\r".  Split on each line.  Convert to a list.
+            List<string> contents = File.ReadAllText("trivia.txt").Replace("\r", "").Split('\n').ToList();
+
+            //Each item in list "contents" is now one line of the Trivia.txt document.
+
+            return new List<Trivia>();
+        }
+    }
+}
